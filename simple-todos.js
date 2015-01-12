4e860e21130275840c4881ae6e45d34b83d72a71
Sanjeev Sekar <sankumsek@gmail.com>
2015-01-12 15:22:29 -0800

    Adding tasks with a form

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,22 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
+  });
+  Template.body.events({
+  	"submit .new-task": function (event) {
+  		var text = event.target.text.value;
+
+  		Tasks.insert({
+  			text: text,
+  			createdAt: new Date() //current time
+  		});
+  		// Clear form
+  		event.target.text.value = "";
+
+  		// Prevent default form submit
+  		return false;
+  	}
   });
 }
