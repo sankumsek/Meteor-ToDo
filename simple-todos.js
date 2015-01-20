397de332357678ece8f8e3259e250d25c09a77c6
Sanjeev Sekar <sankumsek@gmail.com>
2015-01-20 15:23:36 -0800

    Replace Template.body.helpers with new if block

--- simple-todos.js
+++ simple-todos.js
@@ -2,11 +2,20 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+	Template.body.helpers({
+		tasks: function () {
+			if (Session.get("hideCompleted")) {
+				// If hide completed is checked, filter tasks
+				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+			} else {
+				// Otherwise, return all of the tasks
+				return Tasks.find({}, {sort: {createdAt: -1}});
+			}
+		},
+		hideCompleted: function () {
+			return Session.get("hideCompleted");
+		}
+	});
   Template.body.events({
   	"submit .new-task": function (event) {
   		var text = event.target.text.value;
