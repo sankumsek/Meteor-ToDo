ed29091539df7c1114786a4fcf24e2dd5d8faa3f
Sanjeev Sekar <sankumsek@gmail.com>
2015-01-20 15:22:50 -0800

    Add event handler for previous checkbox

--- simple-todos.js
+++ simple-todos.js
@@ -21,6 +21,9 @@ if (Meteor.isClient) {
   		// Prevent default form submit
   		return false;
   	}
+	  "change .hide-completed input": function (event) {
+		  Session.set("hideCompleted", event.target.checked);
+	  }
   });
 }
 Template.task.events({
